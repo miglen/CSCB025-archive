f73b9760bd02ee00d4c417aa41c2ae00048c6eb7
Miglen Evlogiev <github@miglen.com>
2017-11-05 20:47:50 +0000

    Add Plates PHP Template engine and config file

--- config.php
+++ config.php
@@ -2,6 +2,28 @@
 #
 # Основен конфигурационен файл
 #
+ini_set( "display_errors", true );
+# error_reporting(E_ALL);
 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'cscb025');
+define('DB_PASS', 'cscb025');
+define('DB_DB', 'cscb025');
+
+define('PWD', dirname(__FILE__));
+define('CLASS_PATH', PWD.'/files/php' );
+define('TEMPLATE_PATH', PWD.'/files/templates' );
+define('CSS_PATH', PWD.'/files/css' );
+define('JS_PATH', PWD.'/files/js' );
+
+function exception_handler($exception) {
+  echo("Sorry, a problem occurred. Please try later.");
+  error_log($exception->getMessage());
+}
+
+set_exception_handler('exception_handler');
+
+# Включване на темплейти (Platesphp.com)
+require_once(CLASS_PATH.'/plates/Engine.php');
 
 ?>
--- index.php
+++ index.php
@@ -3,4 +3,15 @@
 # Index
 #
 
+# Конфигурационен файл
+require_once('./config.php');
+
+# Create new Plates instance
+$templates = new League\Plates\Engine('./files/php/templates');
+
+# Preassign data to the layout
+$templates->addData(['company' => 'Име на компанията'], 'layout');
+
+#  Render a template
+echo $templates->render('profile', ['name' => 'Вашето име']);
 ?>
